9b69dc2a4cac69bd788a0f5756efa65a84682ee6
Sarabjot Singh <sarabjotsingh@Sarabjots-MacBook-Pro.local>
2026-07-10 16:55:52 +0530

    Added repository for logging and storing SQL scripts

---
+++ repository/metadata_query.py
@@ -0,0 +1,99 @@
+'''CREATE TABLE metadata.ingestion_log
+(
+    id           SERIAL PRIMARY KEY,
+    file_name    VARCHAR(255) NOT NULL,
+    source       TEXT NOT NULL,
+    destination  TEXT NOT NULL,
+    status       VARCHAR(20) NOT NULL,
+    processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);'''
+
+'''
+CREATE TABLE metadata.silver_load_log
+(
+    id           SERIAL PRIMARY KEY,
+    file_name    VARCHAR(255) NOT NULL,
+    source       TEXT NOT NULL,
+    destination  TEXT NOT NULL,
+    method       VARCHAR(20) NOT NULL,
+    status       VARCHAR(20) NOT NULL,
+    processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+'''
+
+class QueryStore:
+
+    @staticmethod
+    def ingestion_log(file_name, source, destination, status):
+        query = f'''
+            INSERT INTO metadata.ingestion_log
+            (
+                file_name,
+                source,
+                destination,
+                status
+            )
+            VALUES
+            (
+                '{file_name}',
+                '{source}',
+                '{destination}',
+                '{status}'
+            );
+        '''
+
+        return query
+
+    @staticmethod
+    def silver_load_log(file_name, source, destination, status, method='overwrite'):
+        query = f'''
+            INSERT INTO metadata.silver_load_log
+            (
+                file_name,
+                source,
+                destination,
+                method,
+                status
+            )
+            VALUES
+            (
+                '{file_name}',
+                '{source}',
+                '{destination}',
+                '{method}',
+                '{status}'
+            );
+        '''
+
+        return query
+    
+    @staticmethod
+    def get_n_latest_files(n):
+        query = f'''
+            WITH top_n_files AS (
+                SELECT 
+                    *
+                    , ROW_NUMBER() OVER(ORDER BY processed_at DESC) AS rn 
+                FROM metadata.ingestion_log
+                WHERE status = 'SUCCESS'
+            )
+            SELECT 
+                DISTINCT destination 
+            FROM top_n_files
+            WHERE rn <= {n};
+        '''
+
+        return query
+    
+    @staticmethod
+    def get_successful_bronze_files():
+        query = '''
+            SELECT
+                DISTINCT destination
+            FROM metadata.ingestion_log
+            WHERE status = 'SUCCESS';
+        '''
+        return query
+
+    def pipeline_run_log():
+        pass
